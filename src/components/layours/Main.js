f5d32f6d9cc693a86991f5181a7eb7212141e247
Mehedi Titu <mh.me.mehedi@gmail.com>
2022-10-25 10:39:21 +0600

    Footer Page done

--- src/components/layours/Main.js
+++ src/components/layours/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Footer';
 import Menu from '../pages/Menu';
 
 const Main = () => {
     return (
         <div>
             <Menu></Menu>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
