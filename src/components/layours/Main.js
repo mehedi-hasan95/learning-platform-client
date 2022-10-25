042c429c03aff8b50aedf4d669bf084691eeb31b
Mehedi Titu <mh.me.mehedi@gmail.com>
2022-10-25 09:50:41 +0600

    Menu design done

---
+++ src/components/layours/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Menu from '../pages/Menu';
+
+const Main = () => {
+    return (
+        <div>
+            <Menu></Menu>
+        </div>
+    );
+};
+
+export default Main;
