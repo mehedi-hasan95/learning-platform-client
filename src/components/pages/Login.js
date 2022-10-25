52536413f30946bbc32b1f10b3d4d8f4f76394ba
mehedititu <mh.me.mehedi@gmail.com>
2022-10-25 19:49:27 +0600

    Login form handle

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
     return (
         <div className= 'w-96 mx-auto'>
             <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-900 text-gray-100">
                 <h1 className="text-2xl font-bold text-center">Login</h1>
-                <form action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
+                <form onSubmit={handleLogin} className="space-y-6 ng-untouched ng-pristine ng-valid">
                     <div className="space-y-1 text-sm">
-                        <label className="block dark:text-gray-400">Username</label>
-                        <input type="text" name="username" id="username" placeholder="Username" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 text-black focus:dark:border-violet-400" />
+                        <label className="block dark:text-gray-400">Your Email</label>
+                        <input type="email" name="email" id="username" placeholder="Your Email" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 text-black focus:dark:border-violet-400" />
                     </div>
                     <div className="space-y-1 text-sm">
-                        <label className="block dark:text-gray-400">Password</label>
-                        <input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 text-black focus:dark:border-violet-400" />
+                        <label className="block dark:text-gray-400">Your Password</label>
+                        <input type="password" name="password" id="password" placeholder="Your Password" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 text-black focus:dark:border-violet-400" />
                         <div className="flex justify-end text-xs dark:text-gray-400">
                             <a rel="noopener noreferrer" href="#">Forgot Password?</a>
                         </div>
