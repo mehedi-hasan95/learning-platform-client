21787e4cebb385cf8ae4c58ae25f916040ddfecb
mehedititu <mh.me.mehedi@gmail.com>
2022-10-25 07:43:56 +0600

    Install firebase

---
+++ src/components/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCK4mr_U98P7YVPvuijckw7ao-BlsZVsnM",
+  authDomain: "learning-platform-16bc6.firebaseapp.com",
+  projectId: "learning-platform-16bc6",
+  storageBucket: "learning-platform-16bc6.appspot.com",
+  messagingSenderId: "413300451294",
+  appId: "1:413300451294:web:b585bf3fb10df969981f4f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
