b6d909721d3811ccf7a5585ccd8fe33218af8909
mehedititu <mh.me.mehedi@gmail.com>
2022-10-26 17:15:50 +0600

    Trying after the error

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -21,7 +21,7 @@ const Menu = () => {
                         <li className="flex">
                             <NavLink rel="noopener noreferrer" to='/home' className="flex items-center px-4 -mb-1 dark:border-transparent">Home</NavLink>
                         </li>
-                        
+
                         <li className="flex">
                             <NavLink rel="noopener noreferrer" to='/course' className="flex items-center px-4 -mb-1 dark:border-transparent">Course</NavLink>
                         </li>
@@ -31,24 +31,17 @@ const Menu = () => {
                         <li className="flex">
                             <NavLink rel="noopener noreferrer" to='/blog' className="flex items-center px-4 -mb-1 dark:border-transparent">Blog</NavLink>
                         </li>
-<<<<<<< HEAD
                         {user?.uid &&
                             <li className="flex">
                                 <div class="relative before:z-10 before:bg-gray-700 before:px-2 before:py-1.5 before:text-white before:invisible before:content-[attr(data-tip)] hover:before:visible" data-tip={user.displayName}>
-                                {
-                                    user?.photoURL ?
-                                        <NavLink rel="noopener noreferrer" className="flex items-center px-4 -mb-1 dark:border-transparent"><img className=' rounded-full w-[2rem]' src={user.photoURL} alt="" /></NavLink>
-                                        :
-                                        <NavLink rel="noopener noreferrer" className="flex items-center px-4 -mb-1 dark:border-transparent"><FaUserAlt className=' rounded-full w-[2rem]' /></NavLink>
-                                }
+                                    {
+                                        user?.photoURL ?
+                                            <NavLink rel="noopener noreferrer" className="flex items-center px-4 -mb-1 dark:border-transparent"><img className=' rounded-full w-[2rem]' src={user.photoURL} alt="" /></NavLink>
+                                            :
+                                            <NavLink rel="noopener noreferrer" className="flex items-center px-4 -mb-1 dark:border-transparent"><FaUserAlt className=' rounded-full w-[2rem]' /></NavLink>
+                                    }
                                 </div>
                             </li>
-=======
-                        { user?.uid && 
-                        <li className="flex">
-                            <NavLink rel="noopener noreferrer" to='/blog' className="flex items-center px-4 -mb-1 dark:border-transparent"><img className=' rounded-full w-[2rem]' src={user.photoURL} alt="" /></NavLink>
-                        </li>
->>>>>>> 61f65ec2f57e41300730ed0575bb6cee94dfcf87
                         }
                         {
                             user?.uid ?
