ffb61162f4370c0a7a88322927f3b8d477aa19ab
mehedititu <mh.me.mehedi@gmail.com>
2022-10-26 17:53:51 +0600

    Add category page's sidebar

--- src/components/pages/Course.js
+++ src/components/pages/Course.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import SingleCourse from './SingleCourse';
 
 const Course = () => {
@@ -10,11 +11,14 @@ const Course = () => {
     }, []);
     return (
         <div className='grid grid-cols-3 gap-4'>
-            <div>
-                <h2>Sidebar</h2>
+            <div className='bg-gray-800 p-5'>
+                <h2 className='text-violet-400 text-2xl pb-5 font-bold'>All Courses</h2>
+                {
+                    categories.map(category => <p className='pb-4 text-xl text-white hover:text-violet-300 font-semibold' key={category.id}> <Link to={`/course/${category.id}`}>{category.name}: {category.title}</Link> </p>)
+                }
             </div>
             <div className='col-span-2'>
-                <div className='grid md:grid-cols-2 px-4 py-8 lg:grid-cols-3 gap-6 bg-gray-800 text-gray-100'>
+                <div className='grid md:grid-cols-2 px-4 py-8 lg:grid-cols-3 gap-6 bg-gray-400 text-gray-100'>
                     {
                         categories.map(category => <SingleCourse key={category.id} category={category}></SingleCourse>)
                     }
