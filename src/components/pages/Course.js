1f9dd159281b554c67ae23a6bcd0883f47382af5
mehedititu <mh.me.mehedi@gmail.com>
2022-10-26 20:04:03 +0600

    Price and other done

--- src/components/pages/Course.js
+++ src/components/pages/Course.js
@@ -10,18 +10,29 @@ const Course = () => {
             .then(data => setCategories(data))
     }, []);
     return (
-        <div className='grid grid-cols-3 gap-4'>
-            <div className='bg-gray-800 p-5'>
-                <h2 className='text-violet-400 text-2xl pb-5 font-bold'>All Courses</h2>
-                {
-                    categories.map(category => <p className='pb-4 text-xl text-white hover:text-violet-300 font-semibold' key={category.id}> <Link to={`/course/${category.id}`}>{category.name}: {category.title}</Link> </p>)
-                }
+        <div>
+            <div className="bg-[url('https://img.freepik.com/free-vector/online-tutorials-concept_52683-37481.jpg?w=1380&t=st=1666785899~exp=1666786499~hmac=8393a5218a40b29e5138d5a5ce0f195ed8e28a897837baa74d3fda447879b2f7')] relative before:absolute before:top-0 before:h-full before:w-full before:contents('') before:bg-black before:opacity-50 mb-7">
+                <section className='relative z-30'>
+                    <div className="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
+                        <h1 className="text-4xl font-bold leading-none sm:text-5xl text-white">Start your course right now!!! Enjoy your learning
+                        </h1>
+                        <p className="px-8 mt-8 mb-12 text-lg text-violet-300 font-semibold">Learn with us. You will learn, we'll assist. Go ahed</p>
+                    </div>
+                </section>
             </div>
-            <div className='col-span-2'>
-                <div className='grid md:grid-cols-2 px-4 py-8 lg:grid-cols-3 gap-6 bg-gray-400 text-gray-100'>
+            <div className='grid grid-cols-3 gap-4'>
+                <div className='bg-gray-800 p-5'>
+                    <h2 className='text-violet-400 text-2xl pb-5 font-bold'>All Courses</h2>
                     {
-                        categories.map(category => <SingleCourse key={category.id} category={category}></SingleCourse>)
+                        categories.map(category => <p className='pb-4 text-xl text-white hover:text-violet-300 font-semibold' key={category.id}> <Link to={`/course/${category.id}`}>{category.name}: {category.title}</Link> </p>)
                     }
+                </div>
+                <div className='col-span-2'>
+                    <div className='grid md:grid-cols-2 px-4 py-8 lg:grid-cols-3 gap-6 bg-gray-400 text-gray-100'>
+                        {
+                            categories.map(category => <SingleCourse key={category.id} category={category}></SingleCourse>)
+                        }
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/pages/CourseDetails.js
+++ src/components/pages/CourseDetails.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const CourseDetails = () => {
-    const details = useLoaderData();
-    console.log(details);
+    const courseDetails = useLoaderData();
+    const { author, category_name, details, image_url, price, rating, thumbnail_url, title, total_student } = courseDetails;
+    console.log(courseDetails);
     return (
         <div>
-            <h2>Details</h2>
+            <div style={{ backgroundImage: `url(${image_url})` }} className="mb-6 relative py-28 md:py-52 lg:py-60 before:absolute before:top-0 before:h-full before:w-full before:contents('') before:bg-black before:opacity-50 mb-7">
+                <h2 className='text-center text-white text-4xl font-extrabold tracking-widest relative z-30'>Learn: <span className='text-violet-400'>{category_name}</span></h2>
+            </div>
+            <div>
+                <section className="bg-gray-800 text-gray-100">
+                    <div className="flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+                        <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+                            <h1 className="text-5xl font-bold leading-none sm:text-6xl">{title}</h1>
+                            <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start mt-5">
+                                <Link rel="noopener noreferrer" to='' className="px-8 py-3 text-lg font-semibold rounded bg-violet-400 hover:bg-violet-700 transition duration-500 text-gray-900 hover:text-white">Buy Now</Link>
+                            </div>
+                        </div>
+                        <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                            <img src={thumbnail_url} alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
+                        </div>
+                    </div>
+                </section>
+                <section className='grid grid-cols-3 gap-4 bg-gray-300 p-5'>
+                    <div className='col-span-2'>
+                        <div>
+                            <img src={author.img} className="h-16 rounded-full mb-3" alt="" />
+                            <h4 className='text-xl mb-2'>Author: {author.name}</h4>
+                            <h4 className='text-lg'>Published: {author.published_date}</h4>
+                        </div>
+                        <h2 className='text-2xl my-5 text-violet-400 font-bold'>Details about this course</h2>
+                        <p>{details}</p>
+                    </div>
+                    <div className='bg-[#FDF9F8] p-5 flex flex-col gap-4'>
+                        <h3 className='text-xl'>{title}</h3>
+                        <h4 className='text-lg'> Price: ${price}</h4>
+                        <h4 className='text-lg'>Duration: 4 weeks</h4>
+                        <h4 className='text-lg'>Level: All levels</h4>
+                        <h4 className='text-lg'>Lectures: 4 lesson</h4>
+                        <h4 className='text-lg'>Rating: {rating.number}</h4>
+                        <h4 className='text-lg'>Enrolled: {total_student}</h4>
+                        <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start mt-5">
+                            <Link rel="noopener noreferrer" to='' className="px-8 py-3 text-lg font-semibold rounded bg-violet-400 hover:bg-violet-700 transition duration-500 text-gray-900 hover:text-white">Buy Now</Link>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 };
